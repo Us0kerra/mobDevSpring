44fdba4bca65e482b3569de4775ee5b7ec87bf41
Us0kerra <stesab8@gmail.com>
2025-02-16 22:21:24 +0300

    Release/lab-2 оптимизация кода

--- app/src/main/java/com/example/firststeps/MainActivity.kt
+++ app/src/main/java/com/example/firststeps/MainActivity.kt
@@ -47,11 +47,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         aInputTextView = findViewById(R.id.editTextA)
         bInputTextView = findViewById(R.id.editTextB)

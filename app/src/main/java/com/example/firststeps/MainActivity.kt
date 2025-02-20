904acb7c2a5f57925ed7ae3c9c9e6b98d22d29ca
Us0kerra <stesab8@gmail.com>
2025-02-20 17:25:13 +0300

    Release/lab-1 убран юмор. Около ТЗ

--- app/src/main/java/com/example/firststeps/MainActivity.kt
+++ app/src/main/java/com/example/firststeps/MainActivity.kt
@@ -19,23 +19,34 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cInputTextView: TextView
 
     private fun integersNotCorrect(a: Double, b: Double, c:Double): Boolean{
-        if (a.isNaN() || b.isNaN() || c.isNaN()) {return true}
         val D: Double = b*b - 4*a*c
         return (D < 0) || (a.toInt() == 0 && b.toInt() == 0) || (a.toInt() == 0 && b.toInt() == 0 && c.toInt() == 0)
     }
 
-    fun calcQuadro(a: Double, b: Double, c:Double): String{
+    fun uncorrectEnter(a:String,b:String,c:String):Boolean{
+        return (a.toDoubleOrNull() == null || b.toDoubleOrNull() == null || c.toDoubleOrNull() == null)
+    }
+
+    fun calcQuadro(a: String, b: String, c:String): String{
         var result1:Double = 0.0
         var result2:Double = 0.0
 
-        if (!integersNotCorrect(a,b,c)){
+        var aDouble = a.toDouble()
+        var bDouble = b.toDouble()
+        var cDouble = c.toDouble()
+
+        if (uncorrectEnter(a,b,c)){
+            return "Некорректный ввод"
+        }
+
+        if (!integersNotCorrect(aDouble,bDouble,cDouble)){
             if (a.toInt() == 0){
-                val result = -c / b
-                return "x = $result"
+                val result = -cDouble / bDouble
+                return "Это линейное уравнение. x = $result"
             }
-            val D = b*b - 4*a*c
-            result1 = (-b + sqrt(D)) / (2*a)
-            result2 = (-b - sqrt(D)) / (2*a)
+            val D = bDouble*bDouble - 4*aDouble*cDouble
+            result1 = (-bDouble + sqrt(D)) / (2*aDouble)
+            result2 = (-bDouble - sqrt(D)) / (2*aDouble)
             return "x1 = $result1, x2 = $result2"
         }
         return "Корней нет"
@@ -54,9 +65,9 @@ class MainActivity : AppCompatActivity() {
         val alert1 = AlertDialog.Builder(this).setPositiveButton("Понял", { d, id->d.cancel() } )
 
         button.setOnClickListener{
-            var aInput = aInputTextView.text.toString().replace(',','.').toDouble()
-            var bInput = bInputTextView.text.toString().replace(',','.').toDouble()
-            var cInput = cInputTextView.text.toString().replace(',','.').toDouble()
+            var aInput = aInputTextView.text.toString().replace(',','.')
+            var bInput = bInputTextView.text.toString().replace(',','.')
+            var cInput = cInputTextView.text.toString().replace(',','.')
             var quadroResult = calcQuadro(aInput,bInput,cInput)
 
             alert1.setMessage(quadroResult).create()
